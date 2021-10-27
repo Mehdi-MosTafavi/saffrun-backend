31ba270f338500eab938e60b0a95c3212051de9d
Mehdi Mostafavi <seyedmehdimostafavi79@gmail.com>
2021-10-27 21:09:15 +0330

    Fix cors origin problem

--- saffrun/saffrun/settings.py
+++ saffrun/saffrun/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "authentication",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -136,3 +138,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
