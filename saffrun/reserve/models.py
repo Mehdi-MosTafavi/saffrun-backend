0b864cc7ba872a23bcafca94cf32a73fddadb2dc
AliBagherz <bagherzadeh007@gmail.com>
2021-11-04 09:01:12 +0330

    make models and its migrations

--- saffrun/reserve/models.py
+++ saffrun/reserve/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from saffrun.commons.BaseModel import BaseModel
+
+
+class Reservation(BaseModel):
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    capacity = models.PositiveIntegerField()
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="owned_reserves"
+    )
+    participants = models.ManyToManyField(
+        User, related_name="participated_reserves"
+    )
