a9e735c5b0fc1474e3159c7b7940b80e669abab4
shnaseri <57701223+shnaseri@users.noreply.github.com>
2021-12-31 21:25:26 +0330

    edit api and return business title

--- saffrun/profile/serializers.py
+++ saffrun/profile/serializers.py
@@ -10,6 +10,7 @@ class FollowSerializer(serializers.Serializer):
 class EmployeeProfileSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField("get_full_name")
     username = serializers.SerializerMethodField("get_username")
+    title = serializers.SerializerMethodField("get_title")
 
     @staticmethod
     def get_full_name(user_profile):
@@ -23,11 +24,15 @@ class EmployeeProfileSerializer(serializers.ModelSerializer):
     def get_username(user_profile):
         return user_profile.user.username
 
+    @staticmethod
+    def get_title(user_profile):
+        print(user_profile.business)
+        return user_profile.business.title if user_profile.business is not None else user_profile.user.last_name
+
     class Meta:
         model = UserProfile
-        fields = ["id", "username", "full_name"]
+        fields = ["id", "username", "full_name", "title"]
 
 
 class RemoveFollowerSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
-
