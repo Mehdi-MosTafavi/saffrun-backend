3b211692b57939deaf3bb3e2d24d646a789bedb9
shnaseri <57701223+shnaseri@users.noreply.github.com>
2022-01-01 18:20:12 +0330

    edit mode in url

--- saffrun/payment/urls.py
+++ saffrun/payment/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'payment'
 
 urlpatterns = [
-    path('', views.Payment.as_view(), name="payment"),
+    path('', views.Payment.as_view(mode=None), name="payment"),
     path('event/', views.Payment.as_view(mode="event"), name="payment_event"),
     path('reserve/', views.Payment.as_view(mode="reserve"), name="payment_reserve"),
     path('event/<int:mode_id>/', views.Payment.as_view(mode="event"), name="payment_get_reserve"),
